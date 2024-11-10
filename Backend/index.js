f9da5a422639bb3ddb9e62ee576da878a7746b43
Akshay10258 <akshayachar1913518@gmail.com>
2024-11-10 12:05:53 +0530

    Updated some frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const cors = require("cors")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require('express');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require('path');
-const { connectomongodb}=require("./connect")
-const app = express()
-const port = 3000
-
+const {connectomongodb}=require("./connect");
+const app = express();
+const port = 3000;
 
 connectomongodb("mongodb://localhost:27017/BeMyPg")
 .then(()=>console.log("Mongodbconnected"));
@@ -17,10 +16,11 @@ const staticRoute=require("./routes/staticRouter");
 const pgowner=require("./routes/Pgowner");
 const pguser=require("./routes/Pguser");
 const AddNewRoomRoute = require("./routes/AddNewRoom");
-const ViewPgDetailsRoute = require("./routes/ViewPgDetails")
+const ViewPgDetailsRoute = require("./routes/ViewPgDetails");
 const OwnerProfile = require("./routes/OwnerProfile.js");
 const UserProfile = require("./routes/UserProfile.js");
 const UserFindPgByCity = require("./routes/UserFindPgByCity.js");
+
 // Midleware for packages
 app.use(cors())
 app.use(bodyParser.json())
@@ -44,7 +44,6 @@ app.use("/owner",pgowner);      // means if url with /owner then call this
 app.use("/user",pguser);
 // MongoDB connection : BeMyPg (Name of the database)
 // mongoose.connect("mongodb://localhost:27017/BeMyPg")
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
